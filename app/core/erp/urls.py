0dabd5ea50f3751f6a380ce76c8822ea60e21151
stiven <adisjimenez@misena.edu.co>
2022-01-21 09:16:05 -0500

    desde ahora coemnzamos

--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from core.erp.views.category.views import category_list, CategoryListViews
 
 urlpatterns = [
-    path('list', category_list),
-    path('listviews', CategoryListViews.as_view())
+    path('list', category_list, name='category_list2'),
+    path('listviews', CategoryListViews.as_view(), name='category_list')
 ]
--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView
 
 from core.erp.models import Category
@@ -16,8 +20,16 @@ class CategoryListViews(ListView):
     model = Category
     template_name = 'category/list.html'
 
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
     # def get_queryset(self):
     #     return Category.objects.filter(name__startswith='e')
+
+    def post(self, request, *args, **kwargs):
+        data = {'name' : 'adrian'}
+        return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
